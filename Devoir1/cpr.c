a9ead028d663fdcbbb5b8bf1a22752273a187a89
Vekshan <vbund062@uottawa.ca>
2020-05-27 21:06:15 -0400

    Added Devoir1

---
+++ Devoir1/cpr.c
@@ -0,0 +1,70 @@
+/*------------------------------------------------------------
+Fichier: cpr.c
+
+Nom:
+Numero d'etudiant:
+
+Description: Ce programme contient le code pour la creation
+             d'un processus enfant et y attacher un tuyau.
+	     L'enfant envoyera des messages par le tuyau
+	     qui seront ensuite envoyes a la sortie standard.
+
+Explication du processus zombie
+(point 5 de "A completer" dans le devoir):
+
+	(s.v.p. completez cette partie);
+
+-------------------------------------------------------------*/
+#include <stdio.h>
+#include <sys/select.h>
+
+/* Prototype */
+void creerEnfantEtLire(int );
+
+/*-------------------------------------------------------------
+Function: main
+Arguments: 
+	int ac	- nombre d'arguments de la commande
+	char **av - tableau de pointeurs aux arguments de commande
+Description:
+	Extrait le nombre de processus a creer de la ligne de
+	commande. Si une erreur a lieu, le processus termine.
+	Appel creerEnfantEtLire pour creer un enfant, et lire
+	les donnees de l'enfant.
+-------------------------------------------------------------*/
+
+int main(int ac, char **av)
+{
+    int numeroProcessus; 
+
+    if(ac == 2)
+    {
+       if(sscanf(av[1],"%d",&numeroProcessus) == 1)
+       {
+           creerEnfantEtLire(numeroProcessus);
+       }
+       else fprintf(stderr,"Ne peut pas traduire argument\n");
+    }
+    else fprintf(stderr,"Arguments pas valide\n");
+    return(0);
+}
+
+
+/*-------------------------------------------------------------
+Function: creerEnfantEtLire
+Arguments: 
+	int prcNum - le numero de processus
+Description:
+	Cree l'enfant, en y passant prcNum-1. Utilise prcNum
+	comme identificateur de ce processus. Aussi, lit les
+	messages du bout de lecture du tuyau et l'envoie a 
+	la sortie standard (df 1). Lorsqu'aucune donnee peut
+	etre lue du tuyau, termine.
+-------------------------------------------------------------*/
+
+void creerEnfantEtLire(int prcNum)
+{
+
+    /* S.V.P. completez cette fonction selon les
+       instructions du devoirs. */
+}
